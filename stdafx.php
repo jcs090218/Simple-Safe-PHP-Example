1b54077b93d6a2df95ad49c8babfbe712dda21f1
jenchieh <jcs090218@gmail.com>
2017-11-22 17:25:10 +0800

    add ref header for testing lib files.

---
+++ stdafx.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * $File: stdafx.php $
+ * $Date: 2017-11-22 16:58:44 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright (c) 2017 by Shen, Jen-Chieh $
+ */
+
+
+// Include SSP library.
+include_once("./lib/SSP/ssp.php");
+
+?>
