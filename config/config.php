14096498bae71b713fd44a43b7f9b7850dcfcd24
jenchieh <jcs090218@gmail.com>
2017-11-22 17:26:26 +0800

    add config for future database config.

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * $File: config.php $
+ * $Date: 2017-11-22 16:58:14 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright (c) 2017 by Shen, Jen-Chieh $
+ */
+
+
+
+
+?>
