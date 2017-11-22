1b3f2141beacdc1e96ff90e362c8c67308e74f3a
jenchieh <jcs090218@gmail.com>
2017-11-22 17:25:59 +0800

    add src.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * $File: index.php $
+ * $Date: 2017-11-14 16:37:15 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright (c) 2017 by Shen, Jen-Chieh $
+ */
+
+
+
+include_once("./stdafx.php");
+
+echo "Hello World";
+
+SSP\error("This is error log.");
+SSP\warning("This is warning log.");
+
+
+?>
